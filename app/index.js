3baa76a770a4f0f816724197117b97956cd09103
mclee3360 <mclee3360@gmail.com>
2019-06-06 12:49:04 -0400

    Add navbar list

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 /**
  * Component has:
@@ -12,7 +13,11 @@ class App extends React.Component
 {
     render()
     {
-        return <div>Hello World</div>
+        return (
+            <div className="container">
+                <Popular />
+            </div>
+        )
     }
 }
 
