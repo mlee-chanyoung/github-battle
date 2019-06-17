396e8fb55696958879c2471c048834c67a1621b3
mclee3360 <mclee3360@gmail.com>
2019-06-17 16:59:18 -0400

    Create theme switcher

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import {ThemeProvider} from "./contexts/theme"
+import Nav from "./components/Nav"
 
 /**
  * Component has:
@@ -12,12 +14,32 @@ import Battle from './components/Battle'
  */
 class App extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            theme: "light",
+            toggleTheme: () => (
+                this.setState(({theme}) => ({
+                    theme: theme === "light"
+                        ? "dark"
+                        : "light"
+                }))
+            )
+        }
+    }
     render()
     {
         return (
-            <div className="container">
-                <Battle />
-            </div>
+            <ThemeProvider value={this.state}>
+                <div className={this.state.theme}>
+                    <div className="container">
+                        <Nav />
+                        <Popular />
+                    </div>
+                </div>
+            </ThemeProvider>
         )
     }
 }
